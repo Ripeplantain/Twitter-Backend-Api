4f1f40c70ece4ec68582174a70c46cc6fceeb4c9
ripeplantain <irvingmanny@gmail.com>
2024-01-07 21:43:29 +0000

    project started

---
+++ Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("*")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+{
+    app.UseDeveloperExceptionPage();
+} else {
+    app.UseExceptionHandler("/error");
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.MapControllers();
+
+
+app.MapGet("/error", () => Results.Problem());
+
+app.Run();
+
